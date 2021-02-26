2b88dfb4660b307640a2ce05a31108db5a0e7df3
tatiana358 <tatiana_358@mail.ru>
2021-02-26 17:42:02 +0300

    refactor: add solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 exports.min = function min (array) {
+    return array && array.length ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
 
-
+  return array && array.length ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
+    return array && array.length ? array.reduce((acc, elem) => acc+elem, 0)/array.length : 0;
 }
